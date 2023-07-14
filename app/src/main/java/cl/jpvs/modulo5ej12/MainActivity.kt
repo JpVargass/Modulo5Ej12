c6d1f23dc7adde66b7a383801a18e7b6ca214646
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-13 22:02:03 -0400

    Inicial

---
+++ app/src/main/java/cl/jpvs/modulo5ej12/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.jpvs.modulo5ej12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
