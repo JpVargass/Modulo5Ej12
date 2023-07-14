c8f737dd4904d5a6cf940e813f09120c43576445
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-14 19:19:43 -0400

    validacion edad

--- app/src/main/java/cl/jpvs/modulo5ej12/MainActivity.kt
+++ app/src/main/java/cl/jpvs/modulo5ej12/MainActivity.kt
@@ -9,3 +9,69 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+fun main() {
+    var usuarios = mutableListOf<Usuario>()
+    println("ingresar cantidad de usuarios")
+    val cantidadUsuarios = readln().toInt()
+    for (i in 1..cantidadUsuarios) {
+        println("ingrese el nombre")
+        var nombre = readln()
+        while (!validarNombre(nombre)){
+            println("nombre invalido, ingrese un nombre valido")
+            nombre = readln()
+        }
+
+        println("ingrese el Apellido")
+        var apellidos = readln()
+        while (!validarApellido(apellidos)){
+            println("nombre invalido, ingrese un nombre valido")
+            apellidos = readln()
+        }
+
+        var edad = readln().toInt()
+         while (!validarEdad(edad)){
+             println("edad invalido, ingrese edad valido")
+             edad = readln().toInt()
+         }
+
+        var correo = readln()
+        var sistemaSalud = readln()
+        var usuario = Usuario(nombre, apellidos, edad, correo, sistemaSalud)
+        usuarios.add(usuario)
+    }
+    for (u in usuarios) {
+        println(u)
+    }
+}
+
+fun validarNombre(nombre: String) : Boolean{
+
+    return nombre.length in 1..20 && nombre.all { it.isLetterOrDigit() }
+}
+
+/*
+probar alternativa
+fun validarNombre(nombre: String) : String{
+
+    return nombre.length in 1..20 && nombre.all { it.isLetterOrDigit() }
+}
+* */
+fun validarApellido(apellido: String): Boolean{
+    return apellido.length in 1..20 && apellido.all { it.isLetterOrDigit() }
+}
+fun  validarEdad(edad:String): Boolean {
+    val onlyNumbers = Regex("\\d+")
+    if( onlyNumbers.matches(edad)){
+        return false
+    }
+    return true
+}
+
+data class Usuario(
+    var nombre : String,
+    var apellidos: String,
+    var edad:Int,
+    var correo: String,
+    var sistemaSalud : String  )
+
